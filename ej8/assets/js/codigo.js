b799ef19082b28c65cc1312a9faf4d5059a8dfe4
juca_18 <jccm668@gmail.com>
2020-10-27 19:56:59 -0700

    Ejercicio 8

---
+++ ej8/assets/js/codigo.js
@@ -0,0 +1,27 @@
+let palabra = prompt("Dame una palabra: ");
+
+if (isPalindrome(palabra)) {
+  alert('La palabra es palindrome');
+}else{
+  alert('La palabra NO es palindrome');
+}
+
+function isPalindrome(cadena) {
+  var palabra = cadena.split(' ').join('').toLowerCase();
+
+  let palabra2 = ''
+  for (let i = palabra.length; i >= 0; i--) {
+    palabra2 = palabra2 + palabra.charAt(i);
+  }
+
+  console.log(palabra)
+  console.log(palabra2)
+  
+
+  if (palabra==palabra2){
+    return true;
+  }
+  return false
+
+ 
+}
