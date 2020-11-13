903f63817a247032e50677ebc6cb84522736a2ff
jccm668 <jccm668@gmail.com>
2020-11-12 22:02:30 -0700

    Php-ejercicio1

---
+++ php-ej1/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>PHP</title>
+    </head>
+    <body>
+        <?php
+            $Nombre = "Juan Carlos Castillo";
+            echo 'Hola mi nombre es '.$Nombre;
+        ?>
+    </body>
+</html>
