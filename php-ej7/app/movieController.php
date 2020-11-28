e88897b3721f20fc4338d9d833b28fae9bd9526d
jccm668 <jccm668@gmail.com>
2020-11-27 21:44:44 -0700

    php-ej7

---
+++ php-ej7/app/movieController.php
@@ -0,0 +1,91 @@
+<?php
+    include "app.php";
+    include "connectionController.php";
+
+    if (isset($_POST['action'])){
+
+        if (isset($_SESSION['token']) && $_POST['token'] == $_SESSION['token'] ) {
+            $movieController = new movieController();
+
+            switch ($_POST['action']) {
+                case 'store':
+                    $title = strip_tags($_POST['title']);
+                    $description = strip_tags($_POST['description']);
+                    $minutes = strip_tags($_POST['minutes']);
+                    $clasification = strip_tags($_POST['clasification']);
+                    $category_id = strip_tags($_POST['category_id']);
+                    $movieController->store($title, $description, $minutes, $clasification, $category_id);
+                break;
+            }
+        }else{
+            $_SESSION['error'] = 'Error de seguridad';
+            header("Location:".$_SERVER["HTTP_REFERER"]);
+        }
+    }
+
+    class MovieController{
+
+        public function get(){
+            $conn = connect();
+            if ($conn->connect_error==false){
+                $query = "select * from movies";
+                $prepared_query = $conn->prepare($query);
+                $prepared_query->execute();
+
+                $results = $prepared_query->get_result();
+                $movies = $results->fetch_all(MYSQLI_ASSOC);
+
+                if (count($movies)>0){
+                    return $movies;
+                }else{
+                    return array();
+                }
+
+            }else{
+                return array();
+            }
+        }
+
+        public function store($title, $description, $minutes, $clasification, $category_id){
+            $conn = connect();
+            if ($conn->connect_error==false){
+
+                if($title!="" && $description!="" && $minutes!="" && $clasification && $category_id){
+                    
+                    $target_path = "../assets/img/movies/"; 
+                    $only_name = $_FILES['cover']['name'];
+                    $new_file_name = $target_path.basename($_FILES['cover']['name']);
+
+                    if (move_uploaded_file($_FILES['cover']['tmp_name'], $new_file_name)){
+  
+                        $query = "insert into movies (title, description, cover, minutes, clasification, category_id) VALUES(?,?,?,?,?,?)";
+
+                        $prepared_query = $conn->prepare($query);
+                        $prepared_query->bind_param('sssisi', $title, $description, $only_name, $minutes, $clasification, $category_id);
+                        if ($prepared_query->execute()){
+                            $_SESSION['success'] = 'El registro se ha guardado correctamente';
+                            header("Location:".$_SERVER["HTTP_REFERER"]);
+                        }else{
+                            $_SESSION['error'] = 'Verifica los datos ingresados';
+                            header("Location:".$_SERVER["HTTP_REFERER"]);
+                        }
+
+                    }else{
+                        $_SESSION['error'] = 'La imagen no fue subida';
+                        header("Location:".$_SERVER["HTTP_REFERER"]);
+                    }
+
+
+                }else{
+                    $_SESSION['error'] = 'verifique la informacion del formulario';
+                    header("Location:".$_SERVER["HTTP_REFERER"]);
+                }
+
+            }else{
+                $_SESSION['error'] = 'verifique la conexion a la base de datos';
+                header("Location:".$_SERVER["HTTP_REFERER"]);
+            }
+        }
+
+    }
+?>
---
+++ php-ej7/categories/index.php
@@ -0,0 +1,189 @@
+<?php
+
+    include "../app/categoryController.php";
+    $categoryController = new categoryController();
+
+    $categories = $categoryController->get();
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    if (!isset($_SESSION) || !isset($_SESSION['id'])){
+        header("Location:"."../login");
+    }
+
+    //echo json_encode($categories);
+?>
+
+<html>
+    <head>
+        <title>Consulta</title>
+        <style type="text/css">
+            table, th, td{
+                border: 1px solid black;
+            }
+            fieldset{
+                weight:100px;
+            }
+            #updateForm{
+                display: none;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            if(isset($_SESSION) && isset($_SESSION['error'])){
+                echo "<h3>Error: ".$_SESSION['error']."</h3>";
+                unset($_SESSION['error']);
+            }
+        ?>
+
+        <table>
+
+            <thead>
+                <th>
+                    #
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Description
+                </th>
+                <th>
+                    Actions
+                </th>
+
+            </thead>
+            <tbody>
+
+            <?php foreach ($categories as $category): ?>
+
+            <tr>
+                <td>
+                    <?= $category['id'] ?>
+                </td>
+                <td>
+                    <?= $category['name'] ?>
+                </td>
+                <td>
+                    <?= $category['description'] ?>
+                </td>
+                <td>
+                    <button onclick="edit(<?= $category['id'] ?>,'<?= $category['name'] ?>','<?= $category['description'] ?>','<?= $category['status'] ?>')">
+                        Edit category
+                    </button>
+
+                    <button style="background-color: red;" onclick="deleted(<?= $category['id'] ?>)">
+                        Remove category
+                    </button>
+                </td>
+                
+            </tr>
+            <?php endforeach ?>
+
+            </tbody>
+
+        </table>
+
+        <form id="storeForm" action="../category" method="POST">
+            <fieldset>
+
+                <legend>
+                    Add new Category
+                </legend>
+
+                <label>
+                    Name
+                </label>
+                <input type="text"  name="name" placeholder="terror" required=""> 
+                <br>
+
+                <label>
+                    Description
+                </label>
+                <textarea placeholder="write here" name="description" rows="5" required=""></textarea>
+                <br>
+
+                <label>
+                    Status
+                </label>
+                <select name="status">
+                    <option> active </option>
+                    <option> inactive </option>
+                </select>
+                <br>
+
+                <button type="submit" >Save Category</button>
+                <input type="hidden" name="action" value="store">
+                <input type="hidden" name="token" value=<?= $_SESSION['token'] ?>>
+
+            </fieldset>
+        </form>
+
+        <form id="updateForm" action="../category" method="POST">
+            <fieldset>
+
+                <legend>
+                    Add new Category
+                </legend>
+
+                <label>
+                    Name
+                </label>
+                <input type="text"  id="name" name="name" placeholder="terror" required=""> 
+                <br>
+
+                <label>
+                    Description
+                </label>
+                <textarea placeholder="write here" id="description" name="description" rows="5" required=""></textarea>
+                <br>
+
+                <label>
+                    Status
+                </label>
+                <select name="status" id="status">
+                    <option> active </option>
+                    <option> inactive </option>
+                </select>
+                <br>
+
+                <button type="submit" >Save Category</button>
+                <input type="hidden" name="action" value="update">
+                <input type="hidden" name="id" id="id">
+                <input type="hidden" name="token" value=<?= $_SESSION['token'] ?>>
+
+            </fieldset>
+        </form>
+
+        <form id="destroyForm" action="../category" method="POST">
+                <input type="hidden" name="action" value="destroy">
+                <input type="hidden" name="id" id="id_destroy">
+        </form>
+
+        <script type="text/javascript">
+            
+            function edit(id, name, description, status){
+                
+                document.getElementById('storeForm').style.display="none";
+                document.getElementById('updateForm').style.display="block";
+
+                document.getElementById('name').value = name;
+                document.getElementById('description').value = description;
+                document.getElementById('status').value = status;
+                document.getElementById('id').value = id;
+            }
+
+            function deleted(id){
+                
+                var confirm = prompt("Si quiere borrar el registro escriba "+id)
+                if (confirm==id){
+                    document.getElementById('id_destroy').value = id;
+                    document.getElementById('destroyForm').submit();
+                }
+            }
+        </script>
+    </body>
+</html>
